cd06bd30e2da19fef7eb4a7e20f4e74faedad996
Mehedi <mehedi25122003@gmail.com>
2025-09-11 13:23:29 +0600

    hello

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'btnMain': '#C7AE6A',
+        'secondaryBtn': '#A8904C',
+        'userBg': '#EADDFF',
+      },
+    },
+  },
+  plugins: [],
+}
